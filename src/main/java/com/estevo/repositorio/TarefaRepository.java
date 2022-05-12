3cf38200e27d5fcb5665abea9c31f0272f09c768
JeffersonEstevo <jefferson.estevo@gmail.com>
2022-05-11 21:20:18 -0300

    versao de entrega do projeto

--- src/main/java/com/estevo/repositorio/TarefaRepository.java
+++ src/main/java/com/estevo/repositorio/TarefaRepository.java
@@ -5,7 +5,7 @@ import com.estevo.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // interface para extender interfaces JPA para acessa BD
+@Repository // interface para extender interfaces JPA para acessar BD
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
     
 }
